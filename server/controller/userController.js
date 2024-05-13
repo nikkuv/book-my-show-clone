d6003cd8a55116f7e0b73abd43f2dcbdfd1c2d90
nikkuv <2000vermasaumya18@gmail.com>
2024-05-13 20:33:52 +0530

    add register api integration

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,22 +2,43 @@ const UserMondal = require("../modal/userModal");
 const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
-    try {
-        const isUserExists = await UserMondal.findOne({ email: req.body.email });
+  try {
+    const isUserExists = await UserMondal.findOne({ email: req.body.email });
 
-        if(isUserExists) {
-            return res.status(200).json({ success: false, message: "User already exists" });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        req.body.password = hashedPassword;
-        const user = new UserMondal(req.body);
-        const response = await user.save();
-        res.status(200).json({ success: true, data: response, message: "Resgistration Successful, please login" });
-    }catch(err) {
-        res.status(500).json({ success: false, message: err.message });
+    if (isUserExists) {
+      return res.status(200).json({ success: false, message: "User already exists" });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+    const user = new UserMondal(req.body);
+    const response = await user.save();
+    res.status(200).json({ success: true, data: response, message: "Resgistration Successful, please login" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 };
 
-module.exports = { registerUser }
+const loginUser = async (req, res) => {
+  try {
+    const isUserExists = await UserMondal.findOne({ email: req.body.email });
+    if (!isUserExists) {
+      return res.status(200).json({ success: false, message: "User doesn't exists" });
+    }
+
+    //validate password
+    const validPassword = await bcrypt.compare(req.body.password, isUserExists.password);
+
+    if (!validPassword) {
+      return res.status(200).json({ success: false, message: "Invalid Password" });
+    } else {
+      res.status(200).json({ success: true, data: isUserExists, message: "Login Successful" });
+    }
+
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
+
+module.exports = { registerUser, loginUser }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 require('dotenv').config();
 const userRouter = require("./routes/userRouter");
 const app = express();
+
+app.use(cors());
 
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
