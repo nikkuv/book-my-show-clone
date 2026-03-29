a927e1f86db8e51a3aa3ce458e80e896b201e2d8
nikkuv <2000vermasaumya18@gmail.com>
2026-03-30 03:06:38 +0530

    add env variables

--- client/services/index.js
+++ client/services/index.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ||
+    "http://localhost:5001",
   // TODO: read about this parameter for CORS
   // automatically sends cookies with every request
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-  }
+  },
 });
 
 export default axiosInstance;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const userRouter = require("./routes/userRouter");
 const movieRouter = require("./routes/movieRouter");
 const theatreRouter = require("./routes/theatreRouter");
@@ -10,10 +10,14 @@ const imageUploadRouter = require("./routes/imageUploadRouter");
 const { swaggerUi, specs } = require("./config/swagger");
 const app = express();
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    credentials: true
-}));
+const allowedOrigins = ["http://localhost:3001", process.env.FRONTEND_URL];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 const dbConfig = require("./config/dbConfig");
@@ -21,11 +25,15 @@ app.use(express.json());
 app.use(express.static("public")); // Serve static files
 
 // Swagger API Documentation
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, {
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
     explorer: true,
-    customCss: '.swagger-ui .topbar { display: none }',
-    customSiteTitle: "BookMyShow Clone API Documentation"
-}));
+    customCss: ".swagger-ui .topbar { display: none }",
+    customSiteTitle: "BookMyShow Clone API Documentation",
+  }),
+);
 
 // API Routes
 app.use("/app/v1/users", userRouter);
@@ -34,16 +42,16 @@ app.use("/app/v1/users/theatres", theatreRouter);
 app.use("/app/v1/bookings", bookingRouter);
 app.use("/app/v1/common", imageUploadRouter);
 
-app.get('/', (req, res) => {
-    res.send(`
+app.get("/", (req, res) => {
+  res.send(`
         <h1>BookMyShow Clone API</h1>
         <p>Welcome to the API server!</p>
         <a href="/api-docs">📚 View API Documentation (Swagger)</a>
     `);
-})
+});
 
 const PORT = Number(process.env.PORT) || 5001;
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-    console.log("API Documentation: http://localhost:" + PORT + "/api-docs");
+  console.log("Server is running on port " + PORT);
+  console.log("API Documentation: http://localhost:" + PORT + "/api-docs");
 });
