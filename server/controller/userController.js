4e04063db0300d541beaf95b349562e0199f76a8
nikkuv <2000vermasaumya18@gmail.com>
2026-03-30 03:13:09 +0530

    add attributes

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -39,7 +39,7 @@ const loginUser = async (req, res) => {
     //validate password
     const validPassword = await bcrypt.compare(
       req.body.password,
-      isUserExists.password
+      isUserExists.password,
     );
 
     if (!validPassword) {
@@ -50,15 +50,15 @@ const loginUser = async (req, res) => {
       const token = jwt.sign(
         { userId: isUserExists._id, isAdmin: isUserExists.isAdmin },
         process.env.JWT_SECRET,
-        { expiresIn: "1d" }
+        { expiresIn: "1d" },
       );
 
       res.cookie("token", token, {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000, // 1 day
         // TODO: read about these parameters for security
-        // secure: true, // Uncomment in production (requires HTTPS)
-        // sameSite: 'strict'
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       });
 
       res.status(200).json({
@@ -76,7 +76,9 @@ const loginUser = async (req, res) => {
 const logoutUser = (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
-    expires: new Date(0)
+    expires: new Date(0),
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
   res.status(200).json({ success: true, message: "Logged out successfully" });
 };
